df8e30c13b1876e6ce9525528fbab95423da3f7b
CaigouGemini <743627185@qq.com>
2019-03-02 10:38:36 +0800

    third commit

--- js/jquery.smimgslide.js
+++ js/jquery.smimgslide.js
@@ -2,20 +2,20 @@
 ;(function(){
 	$.fn.extend({
 		"smimgslide":function(){
-			var thisobj=$(this);
+			var thisobj=$(this);			
 			var smimgbox=thisobj.find('.lovelife-list-smimg-box');
 			var ulwidth=thisobj.find('ul:first').width();
 			var idx=1;
-			//arrows show or hide when hover
+			//arrows show or hide when hover//悬停时箭头显示或隐藏
 			thisobj.hover(function() {
 				thisobj.find('span').show();
 			}, function() {
 				thisobj.find('span').hide();
 			});
-			//event delegation
+			//event delegation//事件委托
 			thisobj.on('click', 'span', function(event) {
 				var classname=$(event.target).attr('class');
-				//load img when click first time
+				//load img when click first time//第一次点击时加载img
 				if(idx){
 					var imgs=thisobj.find('ul:last img');
 					imgs.each(function() {
@@ -23,7 +23,7 @@
 					});
 					idx--;
 				}
-				//left scroll
+				//left scroll//左卷
 				if(classname=="lovelife-list-smimg-leftarrow"){
 					if (!smimgbox.is(':animated')) {
 						thisobj.find('ul:last').insertBefore(thisobj.find('ul:first'));
@@ -33,7 +33,7 @@
 						});
 					}
 				}
-				//right scroll
+				//right scroll//右卷
 				if(classname=="lovelife-list-smimg-rightarrow"){
 					if (!smimgbox.is(':animated')) {
 						smimgbox.animate({left:(-ulwidth-40)+"px"}, "normal",function(){
